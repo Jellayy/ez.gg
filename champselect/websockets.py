963749a487b694582b3975e20c0ed2b8c67a2c7a
Benjamin Huth <benjaminmhuth@gmail.com>
2022-01-26 20:22:46 -0700

    fixed formatting to not be sociopathic

--- champselect/websockets.py
+++ champselect/websockets.py
@@ -3,15 +3,16 @@ import json
 import asyncio
 from champselect import functions
 
+
 async def default_message_handler(data):
     # print(data['eventType'] + ' ' + data['uri'])
     pass
 
 
 async def printing_listener(data):
-
     print(data['eventType'] + ' ' + data['uri'])
     print(json.dumps(data, indent=4, sort_keys=True))
+
 
 async def queue_acceptor(data):
     print(data['eventType'] + ' ' + data['uri'])
@@ -19,18 +20,14 @@ async def queue_acceptor(data):
     await functions.accept_queue(wllp)
 
 
-
-
 async def main():
     global wllp
     wllp = await willump.start()
 
-
-
     all_events_subscription = await wllp.subscribe('OnJsonApiEvent', default_handler=default_message_handler)
 
-    wllp.subscription_filter_endpoint(all_events_subscription, '/lol-matchmaking/v1/ready-check', handler=queue_acceptor)
-
+    wllp.subscription_filter_endpoint(all_events_subscription, '/lol-matchmaking/v1/ready-check',
+                                      handler=queue_acceptor)
 
     while True:
         await asyncio.sleep(10)
