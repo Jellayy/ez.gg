29dff58d9ae65378fa38c2acfc0fdb5df4e6ea9a
Benjamin Huth <benjaminmhuth@gmail.com>
2022-02-02 18:20:19 -0700

    made role select and queue accept work good

--- champselect/websockets.py
+++ champselect/websockets.py
@@ -2,6 +2,8 @@ import willump
 import json
 import asyncio
 from champselect import functions
+import logging
+import preferences
 
 
 async def default_message_handler(data):
@@ -15,9 +17,26 @@ async def printing_listener(data):
 
 
 async def queue_acceptor(data):
-    print(data['eventType'] + ' ' + data['uri'])
-    print(json.dumps(data, indent=4, sort_keys=True))
-    await functions.accept_queue(wllp)
+    try:
+
+        print(data['eventType'] + ' ' + data['data']['playerResponse'])
+        print(json.dumps(data, indent=4, sort_keys=True))
+        if data['data']['playerResponse'] == 'None':
+            print("Accepting queue")
+            await functions.accept_queue(wllp)
+    except:
+        print("lmao idk wtf went wrong, not my problem tbh")
+
+
+async def position_listener(data):
+    try:
+        print("am here")
+        print(data['eventType'] + ' ' + data['uri'])
+        if  data['data']['localMember']['firstPositionPreference'] == "UNSELECTED" or data['data']['localMember'][
+            'secondPositionPreference'] == "UNSELECTED":
+            await functions.select_roles(wllp)
+    except:
+        print("at least it's broken and you know it frikkin gays")
 
 
 async def main():
@@ -28,6 +47,9 @@ async def main():
 
     wllp.subscription_filter_endpoint(all_events_subscription, '/lol-matchmaking/v1/ready-check',
                                       handler=queue_acceptor)
+    wllp.subscription_filter_endpoint(all_events_subscription, '/lol-champ-select/v1/session',
+                                      handler=printing_listener)
+    wllp.subscription_filter_endpoint(all_events_subscription, '/lol-lobby/v2/lobby', handler=position_listener)
 
     while True:
         await asyncio.sleep(10)
@@ -35,7 +57,7 @@ async def main():
 
 if __name__ == '__main__':
     # uncomment this line if you want to see willump complain (debug log)
-    # logging.basicConfig(level=logging.NOTSET)
+    logging.basicConfig(level=logging.NOTSET)
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(main())
--- leaguequeue.py
+++ leaguequeue.py
@@ -17,45 +17,6 @@ async def main():
 
 
 
-    # client = await willump.start()
-    #
-    # #create a lobby
-    # await functions.create_lobby(client)
-    # #select roles
-    # await functions.select_roles(client)
-    # #get lobby type
-    # await functions.queue_type(client)
-    # if await functions.can_start(client) and await functions.is_lobby_leader(client):
-    #     await functions.start_queue(client)
-    # #     while not await functions.queue_pop(client):
-    # #         print("queue has not popped yet")
-    # #         await sleep(5)
-    # #     await functions.accept_queue(client)
-    # #     while not await functions.is_champ_select(client):
-    # #         print("not quite champ select")
-    # #         await sleep(1)
-    # #     print('made it to champ select')
-    # #     await functions.lobby(client)
-    # #     await functions.pick_champ(client, await functions.lobby(client))
-    # #     await functions.lock_in(client, await functions.lobby(client))
-    # #     await runes.set_rune_page(client, "annie")
-    # #
-    # elif not await functions.can_start(client):
-    #     print("Cannot start queue, exiting program")
-
-
-
-
-
-
-    # while True:
-    #     champ_select = await client.request('get', '/lol-champ-select/v1/session')
-    #     champ_select_json = await champ_select.json()
-    #     if list(champ_select_json.keys())[0] != 'errorCode':
-    #         champion = await client.request('get', '/lol-champ-select/v1/current-champion')
-    #         print(id_to_name(str(await champion.json())))
-    #     else:
-    #         print("Not in champ select")
 
 
 if __name__ == '__main__':
