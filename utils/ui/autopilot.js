4bb198f22034f23b74495be5d491518b6750a54c
Jellayy <austinlynnhuffman@gmail.com>
2022-03-02 19:32:06 -0700

    I hate async

--- utils/ui/autopilot.js
+++ utils/ui/autopilot.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
 
-    // Role selector handlers
-    $('#primaryrole').on('change', function() {
-        eel.autopilot_settings('primary_role', this.value);
-    })
-    $('#secondaryrole').on('change', function() {
-        eel.autopilot_settings('secondary_role', this.value);
-    })
+    eel.expose(get_roles);
+    function get_roles() {
+        return [$('#primaryrole').value, $('#secondaryrole').value]
+    }
 
-    // Misc setting handlers
-    $('#queueaccept').on('change', function() {
-        eel.autopilot_settings('queueaccept', $('#queueaccept').is(':checked'));
-    })
-    $('#lockin').on('change', function() {
-        eel.autopilot_settings('lockin', $('#lockin').is(':checked'));
-    })
+    // // Role selector handlers
+    // $('#primaryrole').on('change', function() {
+    //     eel.autopilot_settings('primary_role', this.value);
+    // })
+    // $('#secondaryrole').on('change', function() {
+    //     eel.autopilot_settings('secondary_role', this.value);
+    // })
+
+    // // Misc setting handlers
+    // $('#queueaccept').on('change', function() {
+    //     eel.autopilot_settings('queueaccept', $('#queueaccept').is(':checked'));
+    // })
+    // $('#lockin').on('change', function() {
+    //     eel.autopilot_settings('lockin', $('#lockin').is(':checked'));
+    // })
 
 })
