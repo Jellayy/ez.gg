c361f5daf8543a6af798a9693326b8cca862962d
Jellayy <austinlynnhuffman@gmail.com>
2022-04-06 14:40:43 -0700

    Repurposed ui state engine to disable autopilot functions when there is insufficient input

--- src/champselect/websockets.py
+++ src/champselect/websockets.py
@@ -25,21 +25,22 @@ async def printing_listener(data):
 
 # Handler for Queue Pop Websocket Filter
 async def queue_acceptor(data):
-    try:
-        # Check if Queue is not already accepted
-        if data['data']['playerResponse'] == 'None':
-            print("Queue Acceptor: Queue Pop Detected")
-            # Check if Auto Queue Accept is enabled or in testing mode
-            if testing or eel.get_queue_preference()():
-                print("Queue Acceptor: Auto Accepting Queue...")
-                # Send Queue Accept Request
-                await functions.accept_queue(client)
-            else:
-                print("Queue Acceptor: Auto Queue Accept Not Enabled")
+    if testing or eel.is_autopilot_ready()():
+        try:
+            # Check if Queue is not already accepted
+            if data['data']['playerResponse'] == 'None':
+                print("Queue Acceptor: Queue Pop Detected")
+                # Check if Auto Queue Accept is enabled or in testing mode
+                if testing or eel.get_queue_preference()():
+                    print("Queue Acceptor: Auto Accepting Queue...")
+                    # Send Queue Accept Request
+                    await functions.accept_queue(client)
+                else:
+                    print("Queue Acceptor: Auto Queue Accept Not Enabled")
 
-    except TypeError:
-        # No data given with call
-        print("WARNING: Queue Acceptor called without queue popped (This is normal)")
+        except TypeError:
+            # No data given with call
+            print("WARNING: Queue Acceptor called without queue popped (This is normal)")
 
 
 # TODO: Actually implement auto role selecting properly
@@ -57,60 +58,61 @@ async def queue_acceptor(data):
 
 # Handler for champ select events filter
 async def champ_select(data):
-    try:
-        # Persistent value for tracking rune page status
-        global runes_set
+    if testing or eel.is_autopilot_ready()():
+        try:
+            # Persistent value for tracking rune page status
+            global runes_set
 
-        # TODO: Implement auto champion picks (currently uses old preferences file)
-        # Hover Stage
-        if data['data']['isSelf'] and data['data']['isPickIntenting']:
-            print("Champ Select: Hovering Stage")
-            # Check if Auto Lock In enabled or in testing mode
-            if testing or eel.get_lock_in_preference()():
-                print("Champ Select: Hovering Champion...")
-                await champ_select_functions.hover_champ()
-            else:
-                print("Champ Select: Not Hovering - Auto Lock-In Not Enabled")
+            # TODO: Implement auto champion picks (currently uses old preferences file)
+            # Hover Stage
+            if data['data']['isSelf'] and data['data']['isPickIntenting']:
+                print("Champ Select: Hovering Stage")
+                # Check if Auto Lock In enabled or in testing mode
+                if testing or eel.get_lock_in_preference()():
+                    print("Champ Select: Hovering Champion...")
+                    await champ_select_functions.hover_champ()
+                else:
+                    print("Champ Select: Not Hovering - Auto Lock-In Not Enabled")
 
-        # Banning Stage
-        if data['data']['isSelf'] and data['data']['activeActionType'] == "ban" and data['data'][
-            'banIntentSquarePortratPath'] == "":
-            print("Champ Select: Banning Stage")
-            # Check if Auto Ban enabled or in testing mode
-            if testing or eel.get_auto_ban_preference()():
-                # Send Champion Ban Request
-                print("Champ Select: Auto-Banning Champion...")
-                await champ_select_functions.ban_champ(client)
-            else:
-                print("Champ Select: Auto-Ban Not Enabled")
+            # Banning Stage
+            if data['data']['isSelf'] and data['data']['activeActionType'] == "ban" and data['data'][
+                'banIntentSquarePortratPath'] == "":
+                print("Champ Select: Banning Stage")
+                # Check if Auto Ban enabled or in testing mode
+                if testing or eel.get_auto_ban_preference()():
+                    # Send Champion Ban Request
+                    print("Champ Select: Auto-Banning Champion...")
+                    await champ_select_functions.ban_champ(client)
+                else:
+                    print("Champ Select: Auto-Ban Not Enabled")
 
-        # TODO: Implement auto champion picks (currently uses old preferences file)
-        # Pick Stage
-        if data['data']['isSelf'] and data['data']['activeActionType'] == "pick":
-            print("Champ Select: Pick Stage")
-            if eel.get_lock_in_preference()():
-                await champ_select_functions.pick_champ()
-            else:
-                print("Champ Select: Not Locking In - Auto Lock-In Not Enabled")
+            # TODO: Implement auto champion picks (currently uses old preferences file)
+            # Pick Stage
+            if data['data']['isSelf'] and data['data']['activeActionType'] == "pick":
+                print("Champ Select: Pick Stage")
+                if eel.get_lock_in_preference()():
+                    await champ_select_functions.pick_champ()
+                else:
+                    print("Champ Select: Not Locking In - Auto Lock-In Not Enabled")
 
-            # Reset rune page tracking
-            runes_set = False
+                # Reset rune page tracking
+                runes_set = False
 
-        # Rune Generation
-        if data['data']['isSelf'] and data['data']['isDonePicking'] and not runes_set:
-            if eel.get_runes_preference()():
-                # Get champion
-                pick = data['data']['championName']
-                print("Champ Select: Setting Runes...")
-                await runes.set_rune_page(client, pick)
-                print("Champ Select: Setting Spells...")
-                await sum_spells.set_sum_spells(client, pick)
-                runes_set = True
+            # Rune Generation
+            if data['data']['isSelf'] and data['data']['isDonePicking'] and not runes_set:
+                if eel.get_runes_preference()():
+                    # Get champion
+                    pick = data['data']['championName']
+                    print("Champ Select: Setting Runes...")
+                    await runes.set_rune_page(client, pick)
+                    print("Champ Select: Setting Spells...")
+                    await sum_spells.set_sum_spells(client, pick)
+                    runes_set = True
 
-    except TypeError:
-        print("ERROR: Champ Select: NO DATA - This should never happen tbh")
-    except:
-        print("bad things")
+        except TypeError:
+            print("ERROR: Champ Select: NO DATA - This should never happen tbh")
+        except:
+            print("bad things")
 
 
 # async def report_listener(data):
