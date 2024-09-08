435a0c2a5f3ac29ab053f2d6387ae2d30fad17e5
jellayy <austinlynnhuffman@gmail.com>
2024-09-08 01:35:11 -0700

    feat: node+tailwind

--- rewrite/main.py
+++ rewrite/main.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 import webview
 
-server = Flask(__name__, static_folder='./assets', template_folder='./templates')
+server = Flask(__name__, static_folder='./ui/static', template_folder='./ui/templates')
 
 @server.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@server.route('/exit', methods=['POST'])
+def exit():
+    print("exit called")
+    webview.windows[0].destroy()
+    return '', 204
+
+@server.route('/minimize', methods=['POST'])
+def minimize():
+    print("minimize called")
+    webview.windows[0].minimize()
+    return '', 204
 
 if __name__ == '__main__':
-    webview.create_window('Flask example', server)
-    webview.start()
-
+    webview.create_window('EZ.GG Experimental', server, frameless=True, height=760)
+    webview.start(user_agent='EZ.GG Experimental')
---
+++ rewrite/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./ui/templates/**/*.html",
+    "./ui/static/src/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
